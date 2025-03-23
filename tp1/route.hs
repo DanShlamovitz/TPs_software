1ccd2706a4145dabeabffb72370715429681756a
DanShlamovitz <dshlamovitz@udesa.edu.ar>
2025-03-23 12:06:27 -0300

    seguimos corrigiendo y creando tests (resolver loadT)

--- tp1/route.hs
+++ tp1/route.hs
@@ -1,4 +1,4 @@
-module Route ( Route, newR, inOrderR, inRouteR )
+module Route ( Route, newR, inOrderR, inRouteR, getRoute )
    where
 
 data Route = Rou [ String ] deriving (Eq, Show)
@@ -6,26 +6,23 @@ data Route = Rou [ String ] deriving (Eq, Show)
 newR :: [ String ] -> Route -- construye una ruta según una lista de ciudades
 inOrderR :: Route -> String -> String -> Bool -- indica si la primer ciudad consultada está antes que la segunda ciudad en la ruta
 inRouteR :: Route -> String -> Bool -- indica si la ciudad consultada está en la ruta
+getRoute :: Route -> [ String ] -- devuelve la lista de ciudades de la ruta
 
-newR cities | null(cities) == True = error "The city list has to contain at least one city"
-            | otherwise = Rou cities
+getRoute (Rou cities) = cities
+
+newR cities 
+   | null(cities) == True = error "The city list has to contain at least one city"
+   | otherwise = Rou cities
 
 inRouteR (Rou []) _ = False
 inRouteR (Rou (x:xs)) c1   
    | x == c1   = True
    | otherwise = inRouteR (Rou xs) c1
 
-
-inOrderR (Rou []) _ _ = error "Empty route encountered during inOrderR"
-inOrderR (Rou (x:xs)) c1 c2
-   | not (inRouteR (Rou (x:xs)) c1) = error ("The city " ++ c1 ++ " isn't a part of the provided route")
-   | not (inRouteR (Rou (x:xs)) c2) = error ("The city " ++ c2 ++ " isn't a part of the provided route")
-   | x == c1 = before c1 c2 xs
-   | otherwise = inOrderR (Rou xs) c1 c2
-
-before :: String -> String -> [String] -> Bool
-before _ _ [] = False
-before c1 c2 (c:cities)
-   | c == c1 = elem c2 cities
-   | c == c2 = False
-   | otherwise = before c1 c2 cities
+inOrderR (Rou []) _ _ = error "The route has to contain at least two cities"
+inOrderR (Rou route) c1 c2 
+   | inRouteR (Rou route) c1 == False = error ("The city " ++ c1 ++ " isn't a part of the provided route")
+   | inRouteR (Rou route) c2 == False = error ("The city " ++ c2 ++ " isn't a part of the provided route")
+   | route !! 0 == c1 = True
+   | route !! 0 == c2 = False
+   | otherwise = inOrderR (Rou (drop 1 route)) c1 c2
--- tp1/tests.hs
+++ tp1/tests.hs
@@ -1,6 +1,6 @@
 module Test where
 
--- import Truck
+import Truck
 import Palet
 import Route
 import Stack
@@ -31,26 +31,116 @@ rga = "Rio Gallegos"
 
 -- Palets
 p1 = newP ba 5
+p2 = newP mdq 4
+p3 = newP bhi 3
+p4 = newP tuc 1
+p5 = newP ush 2
+pError = newP ba 1000
+
 testPalet = [
     testF (newP "" 5),
     testF (newP ba (-1)),
-    testF (newP ba 3),
-    testF (destinationP p1),
-    testF (netP p1)
+    not (testF (newP ba 3)),
+    not(testF (destinationP p1)),
+    not (testF (netP p1))
     ]
 
 -- Pruebas de Route
 ruta1 = newR [tuc, sla, ba, mdq, bhi, rga, ush]
 testRoute = [
     testF (newR []),
-    testF (newR [ba, mdq]),
-    testF (inRouteR ruta1 ba == True),
-    testF (inRouteR ruta1 "Cordoba" == False),
-    testF (inOrderR ruta1 ba bhi == True), -- fijarse el caso en donde si estan en oden devuelve false no tira error
-    testF (inOrderR ruta1 mdq ba == True)
+    not (testF (newR [ba, mdq])),
+
+    inRouteR ruta1 "Cordoba" == False,
+    not (inRouteR ruta1 "Cordoba" == True),
+
+    inRouteR ruta1 ba == True,
+    not(inRouteR ruta1 ba == False),
+
+    inOrderR ruta1 ba mdq == True,
+    not(inOrderR ruta1 ba mdq == False),
+
+    not(inOrderR ruta1 mdq ba == True),
+    inOrderR ruta1 mdq ba == False
     ]
 
--- Pruebas de stack
+-- Stack
+-- ruta1 = newR [tuc, sla, ba, mdq, bhi, rga, ush]
+-- p1 = newP ba 5
+-- p2 = newP mdq 4
+-- p3 = newP bhi 3
+-- p4 = newP tuc 1
+-- p5 = newP ush 2
+-- pError = newP ba 1000
+
+s1 = newS 3
+s2 = stackS s1 p1
+s3 = stackS s2 p2
 
 
+testStack = [
+    testF (newS 0),
+    testF (newS (-1)),
+    not (testF (newS 3)),
+    
+    freeCellsS s1 == 3,
+    not (freeCellsS s1 == 0),
 
+    netS s1 == 0,
+    netS s2 == 5,
+    netS s3 == 9,
+    
+    testF(stackS s1 pError),
+    not (testF(stackS s1 p1)),
+
+    holdsS s3 p5 ruta1 == False,
+    holdsS s3 p4 ruta1 == True,
+
+    popS s3 ba == s3,
+    popS s3 mdq == s2
+    ]
+
+    
+-- Truck
+
+-- ruta1 = newR [tuc, sla, ba, mdq, bhi, rga, ush]
+-- p1 = newP ba 5
+-- p2 = newP mdq 4
+-- p3 = newP bhi 3
+-- p4 = newP tuc 1
+-- p5 = newP ush 2
+-- pError = newP ba 1000
+
+-- s1 = newS 3
+-- s2 = stackS s1 p1
+-- s3 = stackS s2 p2
+
+t1 = newT 3 3 ruta1
+t2 = loadT t1 p1
+t3 = loadT t2 p2
+
+testTruck = [
+    testF (newT 0 0 ruta1),
+    testF (newT (-1) 0 ruta1),
+    not (testF (newT 3 3 ruta1)),
+
+    netT t1 == 5
+    -- netT t2 == 9
+    
+
+    -- testF (newT (-1) 0 ruta1),
+    -- not (testF (newT 3 3 ruta1)),
+
+    -- freeCellsT t1 == 9,
+    -- not (freeCellsT t1 == 0),
+
+    -- netT t1 == 0,
+    -- netT t2 == 5,
+    -- netT t3 == 9,
+
+    -- testF(loadT t1 pError),
+    -- not (testF(loadT t1 p1)),
+
+    -- unloadT t3 "Cordoba" == t3,
+    -- unloadT t3 ba == t2,
+    ]
