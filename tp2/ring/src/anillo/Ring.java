369ccd29b73c17c7ed657668db132b4f3cbdcbda
felpa16 <101142816+felpa16@users.noreply.github.com>
2025-04-04 18:29:54 -0300

    Implementación del ring usando condicionales

--- tp2/ring/src/anillo/Ring.java
+++ tp2/ring/src/anillo/Ring.java
@@ -1,19 +1,48 @@
 package anillo;
+import java.util.ArrayList;
 
 public class Ring {
+
+    private ArrayList<Object> ring;
+
+    // next tiene que avanzar el current y devolver el ring resultante
     public Ring next() {
-        return null;
+        if (ring.size() <= 1) {
+            // NO ME DEJA DEVOLVER EXCEPTION PORQUE EL TIPO DE LA FUNCION ES Ring! Entonces devuelvo el mismo ring
+            //return new Exception("The ring doesn't have enough elements for there to be a next element.");
+            return this;
+        }
+        Object curr = ring.removeFirst();
+        ring.add(ring.size() - 1, curr);
+        return this;
     }
 
     public Object current() {
-        return null;
+        if (ring.isEmpty()) {
+            return new Exception("The ring is empty, there is no current element.");
+        }
+        return ring.getFirst();
     }
 
+
     public Ring add( Object cargo ) {
-        return null;
+        if (!ring.isEmpty()) {
+            Object curr = ring.removeFirst(); // saco el current
+            ring.addFirst(cargo); // agrego el nuevo
+            ring.addFirst(curr); // vuelvo a poner el current
+        }
+        else {
+            ring.addFirst(cargo);
+        }
+        return this;
     }
 
     public Ring remove() {
-        return null;
+        if (ring.isEmpty()) {
+            // nuevamente no puedo devolver una excepcion porque el tipo de la funcion es Ring, no Object.
+            return this;
+        }
+        ring.removeFirst();
+        return this;
     }
 }
