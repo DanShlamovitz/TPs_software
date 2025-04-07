9ea4269866af1236f1c9dda844bcb02b802fb61a
DanShlamovitz <dshlamovitz@udesa.edu.ar>
2025-04-07 20:21:22 -0300

    segundo paso corregido conlo visto en calse

--- tp2/ring/src/anillo/Ring.java
+++ tp2/ring/src/anillo/Ring.java
@@ -1,59 +1,87 @@
 package anillo;
 
-import java.util.LinkedList;
-
-abstract class Node{
-    public Node next;
-    public Node prev;
-    public Object cargo;
-
+abstract class Node {
     public abstract Node add(Object cargo);
     public abstract Node remove();
     public abstract Node next();
     public abstract Object current();
-
 }
 
 class EmptyNode extends Node {
-    public EmptyNode() {
+    public Node add(Object cargo) {
+        FullNode newNode = new FullNode(cargo);
+        newNode.next = newNode;
+        newNode.prev = newNode;
+        return newNode;
+    }
 
+    public Node remove() {
+        throw new RuntimeException("The ring is empty");
+    }
+
+    public Node next() {
+        throw new RuntimeException("The ring is empty");
+    }
+
+    public Object current() {
+        throw new RuntimeException("The ring is empty");
     }
 }
 
-class NodeWithSomething extends Node {
-    public NodeWithSomething(Object cargo) {}
+class FullNode extends Node {
+    Object cargo;
+    FullNode next;
+    FullNode prev;
+
+    public FullNode(Object cargo) {
+        this.cargo = cargo;
+    }
+
+    public Node add(Object newCargo) {
+        FullNode newNode = new FullNode(newCargo);
+        newNode.next = this;
+        newNode.prev = this.prev;
+        this.prev.next = newNode;
+        this.prev = newNode;
+        return newNode;
+    }
+    public Node remove() {
+        if (this.next == this) {
+            return new EmptyNode();
+        }
+        this.prev.next = this.next;
+        this.next.prev = this.prev;
+        return this.next;
+    }
+
+    public Node next() {
+        return this.next;
+    }
+
+    public Object current() {
+        return this.cargo;
+    }
 }
 
-
 public class Ring {
-
-    private LinkedList<Object> ring = new LinkedList<>();
+    private Node current = new EmptyNode();
 
     public Ring next() {
-        if (ring.isEmpty()) {
-            throw new RuntimeException("The ring is empty");
-        }
-        Object curr = ring.removeFirst();
-        ring.addLast(curr);
+        current = current.next();
         return this;
     }
 
     public Object current() {
-        if (ring.isEmpty()) {
-            throw new RuntimeException("The ring is empty");
-        }
-        return ring.getFirst();
+        return current.current();
     }
 
     public Ring add(Object cargo) {
-        ring.addFirst(cargo);
+        current = current.add(cargo);
         return this;
     }
 
     public Ring remove() {
-        if (!ring.isEmpty()) {
-            ring.removeFirst();
-        }
+        current = current.remove();
         return this;
     }
 }
